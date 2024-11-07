fedcaf05f1242edfb983c08a169c920c05d0f052
formerchild05 <electronicncs@gmail.com>
2024-11-08 02:41:47 +0700

    Add new files

---
+++ JavaFX/src/main/java/com/example/javafx/DatabaseConnect.java
@@ -0,0 +1,15 @@
+package com.example.javafx;
+
+//database connection's information.
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class  DatabaseConnect{
+    private static final String url = "jdbc:mysql://127.0.0.1:3306/beta_data";
+    private static final String DataUsername = "root";
+    private static final String DataPassword = "1234";
+
+    public Connection connect() throws Exception {
+        return DriverManager.getConnection(url, DataUsername, DataPassword);
+    }
+}
